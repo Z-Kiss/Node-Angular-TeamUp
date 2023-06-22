cdc344bf00bce97a485eca125217297a46aee4f5
zotya <zoltankiss9210@gmail.com>
2023-06-22 10:25:40 +0200

    add handleError for solve code duplication

--- backend/src/helpers/index.ts
+++ backend/src/helpers/index.ts
@@ -1,4 +1,5 @@
 import crypto from "crypto";
+import express from "express";
 
 const SECRET_KEY = 'l6RXpN5zR5V1G8fEXZbsafdCX5DvC0i9';
 
@@ -7,3 +8,8 @@ export const saltGenerator = () =>crypto.randomBytes(128).toString('base64')
 export const hashGenerator = (salt: string, password: string)=> {
     return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET_KEY).digest('hex')
 };
+
+export const handleError = (error: Error, res: express.Response) =>{
+    console.log(error);
+    return res.sendStatus(400);
+}
